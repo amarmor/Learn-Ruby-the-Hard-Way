d62f6abde7eabe33153ea411173367d72e4d14a3
Alex Marmor <aamarmor@gmail.com>
2017-09-06 17:30:41 -0400

    create ex45.rb

---
+++ ex45.rb
@@ -0,0 +1,44 @@
+# I'm going to make a turn-based pokemon battle game, like the gameboy games. The only status type will be health. It will be a one on one match. Each turn both pokemon attack. Each pokemon knows only Tackle. It's randomly decided which pokemon attacks first. The attack lowers your opponent's health by 10 points. Health starts at 50 points. When one pokemon's health hits 0, that pokemon loses, and the game is over.
+
+# THINGS
+# - match
+# - health
+# - pokemon
+# - attack
+# - status
+# - defense
+# - moves
+# - tackle
+# - harden
+# - focus
+
+# SORTED THINGS
+# status:
+# - attack
+# - defense
+# - HP
+# moves:
+# - tackle
+# - harden
+# - focus
+# pokemon:
+# - pidgey
+# - rattata
+# move properties
+# -- pp
+# -- effect
+
+class Status
+  def attack
+    
+  end
+end
+
+class Moves
+  def tackle
+    
+  end
+end
+
+class Props
+end
